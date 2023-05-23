6b4da661face341646a6b8d542c64a8fc1675060
EstebanSansart <julianestebansanabria@gmail.com>
2023-05-23 09:30:00 -0500

    feat: :tada: firstCommit
    Creados los archivos php principales

---
+++ templates/header.php
@@ -0,0 +1,18 @@
+<div class="encabezado">
+    <div class="mb-3" id="numFactura">
+        <label for="formGroupExampleInput" class="form-label">Bill Number</label>
+        <input type="number" class="form-control" id="formGroupExampleInput" placeholder="0000">
+    </div>
+    <div class="mb-3" id="fecha">
+        <label for="formGroupExampleInput" class="form-label">Date</label>
+        <input type="date" class="form-control" id="formGroupExampleInput">
+    </div>
+    <div class="mb-3 col-12" id="nombre">
+        <label for="formGroupExampleInput" class="form-label">Full Name</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Johlver Pardo">
+    </div>
+    <div class="mb-3" id="cedula">
+        <label for="formGroupExampleInput" class="form-label">C.C.</label>
+        <input type="number" class="form-control" id="formGroupExampleInput" placeholder="1085345897">
+    </div>
+</div>
